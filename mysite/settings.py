af3dff235578c6679bcf2c8020dac0aa6cd5375d
NoMercy <qawedytui@gmail.com>
2019-11-11 22:31:02 +0200

    add post_list.html and relative changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'wss2($=g4z#)51@6ce)42x@^1w^*t1_)4h@k670r*)jvvo_alg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
